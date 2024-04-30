462c93570573774d3d3843c9886e690dceee4b36
Guro Bokum <jiojiajiu@gmail.com>
2024-04-30 16:14:40 +0300

    feat: create landing page

--- packages/nextjs/app/(site)/page.tsx
+++ packages/nextjs/app/(site)/page.tsx
@@ -1,71 +1,55 @@
 "use client";
 
-import Link from "next/link";
-import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/src/components/scaffold-eth";
 
-const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+export default function HomePage() {
+  // workaround for removing non-auth page blinking
+  // when user has connected wallet we would like to show blank page
+  // until we get the account data
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 150);
+  }, []);
+
+  const account = useAccount();
+  if (isLoading) {
+    return null;
+  }
+
+  if (!account.isConnected) {
+    return <DisconnectedHomePage />;
+  }
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
+          <h1 className="text-center"></h1>
         </div>
       </div>
     </>
   );
-};
+}
 
-export default Home;
+function DisconnectedHomePage() {
+  return (
+    <div className="text-center flex flex-col flex-1 justify-center">
+      <div className="mb-5 -mt-10">
+        <div className="text-2xl">Welcome to</div>
+        <h1 className="text-8xl leading-relaxed">Farcaster Lottery</h1>
+        <p>Here you may find create a new lottery or join an existing one.</p>
+      </div>
+      <ConnectButton.Custom>
+        {({ openConnectModal }) => (
+          <button className="btn btn-wide btn-lg btn-primary mt-10 mx-auto block" onClick={openConnectModal}>
+            Connect Wallet
+          </button>
+        )}
+      </ConnectButton.Custom>
+    </div>
+  );
+}
