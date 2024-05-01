6c45f9297a285e49b2373d53ee2b4940d687067a
Guro Bokum <jiojiajiu@gmail.com>
2024-05-01 16:57:37 +0300

    feat: improve creation form

--- packages/nextjs/app/(site)/_components/CreateLottery.tsx
+++ packages/nextjs/app/(site)/_components/CreateLottery.tsx
@@ -1,6 +1,30 @@
 import { useCallback, useState } from "react";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
+import create from "zustand";
+import { useShallow } from "zustand/react/shallow";
+
+type CreateLotteryState = {
+  title: string;
+  hoursToClose: string;
+  creatorFee: string;
+  deposit: string;
+  setTitle: (title: string) => void;
+  setHoursToClose: (hoursToClose: string) => void;
+  setCreatorFee: (creatorFee: string) => void;
+  setDeposit: (deposit: string) => void;
+};
+
+const useCreateLottery = create<CreateLotteryState>(set => ({
+  title: "",
+  hoursToClose: "0",
+  creatorFee: "",
+  deposit: "",
+  setTitle: (title: string) => set({ title }),
+  setCreatorFee: (creatorFee: string) => set({ creatorFee }),
+  setHoursToClose: (hoursToClose: string) => set({ hoursToClose }),
+  setDeposit: (deposit: string) => set({ deposit }),
+}));
 
 export default function CreateLottery() {
   const [page, setPage] = useState(0);
@@ -8,14 +32,15 @@ export default function CreateLottery() {
     <div className="lg:grid grid-cols-2 px-[5vw] py-[5vh] flex flex-col-reverse">
       <div className="grid grid-rows-5 mt-5 md:mt-0">
         <div className="row-span-4 grid grid-rows-sugrid relative overflow-hidden">
-          <FormPage1 page={page} className="w-full absolute" />
-          <FormPage2 page={page} className="w-full translate-x-full absolute" />
-          <FormPage3 page={page} className="w-full translate-x-full absolute" />
-          <FormPage4 page={page} className="w-full translate-x-full absolute" />
+          <SetTitleForm index={0} page={page} className="w-full absolute" />
+          <SetDateForm index={1} page={page} className="w-full translate-x-full absolute" />
+          <SetCreatorFeeForm index={2} page={page} className="w-full translate-x-full absolute" />
+          <SetDepositForm index={3} page={page} className="w-full translate-x-full absolute" />
+          <ReportForm index={4} page={page} className="w-full translate-x-full absolute" />
         </div>
 
         <div className="text-center">
-          <Controls page={page} setPage={setPage} />
+          <Controls pages={5} page={page} setPage={setPage} />
         </div>
       </div>
       <div className="w-[300px] h-[300px] md:w-[500px] md:h-[500px] relative ">
@@ -25,12 +50,15 @@ export default function CreateLottery() {
   );
 }
 
-function Controls({ page, setPage }: { page: number; setPage: (page: number) => void }) {
-  const onNext = useCallback(() => page < 3 && setPage(page + 1), [page, setPage]);
+function Controls({ page, pages, setPage }: { page: number; pages: number; setPage: (page: number) => void }) {
+  const onNext = useCallback(() => page < pages - 1 && setPage(page + 1), [page, setPage]);
   const onPrev = useCallback(() => page > 0 && setPage(page - 1), [page, setPage]);
 
-  const nextClassName = page == 0 ? "w-36 md:w-64" : "";
+  let nextClassName = page == 0 ? "w-36 md:w-64" : "";
   const prevClassName = page == 0 ? "" : "md:w-36 w-32";
+  const nextTitle = page === pages - 1 ? "Create" : "Next";
+  nextClassName += page === pages - 1 ? "btn-secondary" : "";
+
   return (
     <div className="flex flex-row justify-center">
       {page !== 0 && (
@@ -39,54 +67,160 @@ function Controls({ page, setPage }: { page: number; setPage: (page: number) => 
         </button>
       )}
       <button className={twMerge("btn btn-primary md:btn-lg", nextClassName)} onClick={onNext}>
-        Next &raquo;
+        {nextTitle} &raquo;
       </button>
     </div>
   );
 }
 
-function FormPage1({ page, className }: { page: number; className?: string }) {
-  const showClassName = page === 0 ? "opacity-1" : "opacity-0 -translate-x-full";
+function SetTitleForm({ index, page, className }: { index: number; page: number; className?: string }) {
+  const { title, setTitle } = useCreateLottery(
+    useShallow(state => ({
+      title: state.title,
+      setTitle: state.setTitle,
+    })),
+  );
+  const showClassName = generateTransitionClass(index, page);
 
   return (
     <div className={twMerge("h-full grid grid-rows-4 text-center transition duration-500", className, showClassName)}>
       <h2 className="md:text-3xl row-start-2">Enter lottery name</h2>
       <div className="row-start-3">
-        <input type="text" className="input md:input-lg input-wide" placeholder="" />
+        <input
+          type="text"
+          value={title}
+          className="input md:input-lg input-wide"
+          placeholder=""
+          onChange={e => setTitle(e.target.value)}
+        />
       </div>
     </div>
   );
 }
 
-function FormPage2({ page, className }: { page: number; className?: string }) {
-  let showClassName = page === 1 ? "opacity-1 translate-x-0" : "opacity-0";
-  if (page > 1) {
-    showClassName += " -translate-x-full";
-  }
+function SetDateForm({ index, page, className }: { index: number; page: number; className?: string }) {
+  const { hoursToClose, setHoursToClose } = useCreateLottery(
+    useShallow(state => ({
+      hoursToClose: state.hoursToClose,
+      setHoursToClose: state.setHoursToClose,
+    })),
+  );
+  const showClassName = generateTransitionClass(index, page);
+
   return (
-    <div className={twMerge("row-start-2 text-center transition duration-500", className, showClassName)}>XAXA</div>
+    <div className={twMerge("h-full grid grid-rows-4 text-center transition duration-500", className, showClassName)}>
+      <h2 className="md:text-3xl w-64 row-start-1 mx-auto mt-10">
+        By what time does the lottery close
+        <br />
+        (in hours)?
+      </h2>
+      <div className="row-start-3">
+        <input
+          type="text"
+          value={hoursToClose}
+          className="input md:input-lg input-wide"
+          placeholder=""
+          onChange={e => setHoursToClose(e.target.value)}
+        />
+      </div>
+    </div>
   );
 }
 
-function FormPage3({ page, className }: { page: number; className?: string }) {
-  let showClassName = page === 2 ? "opacity-1 translate-x-0" : "opacity-0";
-  if (page > 2) {
-    showClassName += " -translate-x-full";
-  }
-  if (page < 2) {
-    showClassName += " translate-x-full";
-  }
+function SetCreatorFeeForm({ index, page, className }: { index: number; page: number; className?: string }) {
+  const { creatorFee, setCreatorFee } = useCreateLottery(
+    useShallow(state => ({
+      creatorFee: state.creatorFee,
+      setCreatorFee: state.setCreatorFee,
+    })),
+  );
+  const showClassName = generateTransitionClass(index, page);
+
   return (
-    <div className={twMerge("row-start-2 text-center transition duration-500", className, showClassName)}>XIXI</div>
+    <div className={twMerge("h-full grid grid-rows-4 text-center transition duration-500", className, showClassName)}>
+      <h2 className="md:text-3xl w-64 row-start-1 mx-auto mt-10">What would be your creator fee?</h2>
+      <div className="row-start-3">
+        <input
+          type="text"
+          value={creatorFee}
+          className="input md:input-lg input-wide"
+          placeholder="%"
+          onChange={e => setCreatorFee(e.target.value)}
+        />
+      </div>
+    </div>
   );
 }
 
-function FormPage4({ page, className }: { page: number; className?: string }) {
-  let showClassName = page === 3 ? "opacity-1 translate-x-0" : "opacity-0";
-  if (page < 3) {
-    showClassName += " translate-x-full";
-  }
+function SetDepositForm({ index, page, className }: { index: number; page: number; className?: string }) {
+  const { deposit, setDeposit } = useCreateLottery(
+    useShallow(state => ({
+      deposit: state.deposit,
+      setDeposit: state.setDeposit,
+    })),
+  );
+  const showClassName = generateTransitionClass(index, page);
+
   return (
-    <div className={twMerge("row-start-2 text-center transition duration-500", className, showClassName)}>X0X0</div>
+    <div className={twMerge("h-full grid grid-rows-4 text-center transition duration-500", className, showClassName)}>
+      <h2 className="md:text-3xl w-64 row-start-1 mx-auto mt-10">
+        Whould you like to increase prize pool with some money?
+      </h2>
+      <div className="row-start-3">
+        <input
+          type="text"
+          value={deposit}
+          className="input md:input-lg input-wide"
+          placeholder=""
+          onChange={e => setDeposit(e.target.value)}
+        />
+      </div>
+    </div>
   );
 }
+
+function ReportForm({ index, page, className }: { index: number; page: number; className?: string }) {
+  const { deposit, creatorFee, title, hoursToClose } = useCreateLottery(
+    useShallow(state => ({
+      deposit: state.deposit,
+      creatorFee: state.creatorFee,
+      title: state.title,
+      hoursToClose: state.hoursToClose,
+    })),
+  );
+  const showClassName = generateTransitionClass(index, page);
+
+  return (
+    <div className={twMerge("h-full grid grid-rows-4 text-center transition duration-500", className, showClassName)}>
+      <ul className="w-64 mx-auto md:text-lg md:mt-10">
+        <li className="flex justify-between">
+          <span>Title:</span>
+          <span>{title}</span>
+        </li>
+        <li className="flex justify-between">
+          <span>Close in:</span>
+          <span>{hoursToClose || 0} hours</span>
+        </li>
+        <li className="flex justify-between">
+          <span>Your fee</span>
+          <span>{creatorFee || 0}%</span>
+        </li>
+        <li className="flex justify-between">
+          <span>Starting prize pool:</span>
+          <span>{deposit || 0}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+const generateTransitionClass = (index: number, page: number) => {
+  let className = page === index ? "opacity-1 translate-x-0" : "opacity-0";
+  if (page > index) {
+    className += " -translate-x-full";
+  }
+  if (page < index) {
+    className += " translate-x-full";
+  }
+  return className;
+};
