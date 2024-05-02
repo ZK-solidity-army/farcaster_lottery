87120ff0d706e86c12e70bd2f07beaf9f2a2012b
Guro Bokum <JIoJIaJIu@gmail.com>
2024-05-02 16:33:12 +0300

    feat: implement bet frame (#27)
    * feat: implement bet frame

--- packages/hardhat/contracts/Lottery.sol
+++ packages/hardhat/contracts/Lottery.sol
@@ -15,10 +15,10 @@ contract Lottery is AccessControl {
 
     // defaults to 0.001 Eth
     uint256 public constant TICKET_PRICE = 10 ** 16;
-    uint256 public constant STATER_FEE = 5 ** 15;
-    uint256 public constant DEV_FEE = 1 ** 15;
+    uint256 public constant STATER_FEE = 5 * (10 ** 15);
+    uint256 public constant DEV_FEE = 10 ** 15;
 
-    uint256 public constant totalPice = TICKET_PRICE + STATER_FEE + DEV_FEE;
+    uint256 public constant totalPrice = TICKET_PRICE + STATER_FEE + DEV_FEE;
 
     /// @notice Amount available to withdraw to the lottery starter
     uint256 public starterPool;
