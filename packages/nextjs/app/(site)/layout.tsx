6a5d4238c34cdb46c62ff87ebbaf0a8546b3c47b
Guro Bokum <jiojiajiu@gmail.com>
2024-05-08 03:52:17 +0300

    feat: update metdata

--- packages/nextjs/app/(site)/layout.tsx
+++ packages/nextjs/app/(site)/layout.tsx
@@ -9,9 +9,9 @@ const baseUrl = process.env.VERCEL_URL
   : `http://localhost:${process.env.PORT || 3000}`;
 const imageUrl = `${baseUrl}/thumbnail.jpg`;
 
-const title = "Scaffold-ETH 2 App";
-const titleTemplate = "%s | Scaffold-ETH 2";
-const description = "Built with 🏗 Scaffold-ETH 2";
+const title = "Farcaster Lottery";
+const titleTemplate = "%s | Encode";
+const description = "Encode Solidity Bootcamp";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
